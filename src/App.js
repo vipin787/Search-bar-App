41370f33085240351a6bbbcbf00bbb438eac2eb3
vipin <cvipin787@gmail.com>
2023-04-05 17:23:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+// import logo from './logo.svg'; 
+import { useState } from "react";
 import './App.css';
+import { SearchBar } from "./components/SearchBar";
 
 function App() {
+  
+  const addSum = (Hindi, English) => {
+    
+    let result = Hindi + English
+    return result;
+   }
+   
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="search-bar-container">
+        <SearchBar />
+
+        <div>SearchResults</div>
+      </div>
     </div>
   );
 }
---
+++ src/components/SearchBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { FaSearch } from "react-icons/fa"
+import "./SearchBar.css";
+
+export const SearchBar = () => {
+    return <div className="input-wrapper">
+        <FaSearch id="search-icon" />
+        <input placeholder="Type to search..." />
+    </div>
+};
